081fde3dd221234de11f840d74a94f3fd866c412
Aarush Sharma <aarushsharma@gmail.com>
2021-08-29 23:25:54 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonWeb from './components/Button';
 
-function App() {
+function NavBar() {
   return (
     <div className="App">
-      <p>hi</p>
+      <div className="Elements">
+      <a href="https://youtube.com">Home</a>
+      <a>About</a>
+      <a>Examples</a>
+      <a>Contact</a>
+      </div>
+
+      <div className="Auth">
+        <a className="Login">Log in</a>
+        <a className="SignUp">
+          <p>Sign Up</p>
+          <div className="GoogleLogo"></div>
+        </a>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Upload() {
+  return(
+    <div className="Upload">
+      <div className="InfoBlurb">
+        <h1>Uberprints.</h1>
+        <p>A fast, easy to use, and reliable way for printing custom designs through printers near you!</p>
+        <a>Example prints</a>
+      </div>
+      <div className="UploadDesign"></div>
+    </div>
+  );
+}
+
+function Home() {
+  return(
+    <div className="body">
+      <NavBar/>
+      <Upload/>      
+    </div>
+  );
+}
+
+export default Home;
