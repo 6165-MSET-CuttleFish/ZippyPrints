fe31486684e2db9c6fa84fdf952301bee4162d17
ayuram <54257595+ayuram@users.noreply.github.com>
2021-09-04 23:38:24 -0700

    firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import FileUpload from './Upload';
 import NavBar from './components/NavBar';
+import { firebaseConfig } from './firebaseConfig';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function Lander() {
   return (
