7e1ad31e13cb8be34350ff9ecaaf4c3c1473abcf
maartoon <85377718+maartoon@users.noreply.github.com>
2021-11-14 11:17:51 -0800

    map marker

--- src/views/Discover/Discover.js
+++ src/views/Discover/Discover.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {GoogleMap, useLoadScript, Marker, InfoWindow} from '@react-google-maps/api';
-
+import LoginForm from '../Auth/LoginForm'
 
 export default function Discover() {
 
@@ -21,20 +21,74 @@ export default function Discover() {
       googleMapsApiKey: "AIzaSyD66Pg0s20be-L1lod3A29C8uyehouZREE",
       libraries
   });
+  const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  const onMapClick = React.useCallback((event) => {
+    setMarkers((current) => [...current, 
+    {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+      time: new Date(),
+    },
+  ]);
+}, []);
+
+const mapRef = React.useRef();
+const onMapLoad = React.useCallback((map) => {
+  mapRef.current = map;
+}, [])
 
   if (loadError) return "Error loading map";
   if (!isLoaded) return "Loading map...";
 
   return (
-    
+    //TODO: replace with actual logo
   <div>
+    
+    <h1>
+      Zippyprints{" "}
+      <span role ="img" aria-label="logo">
+      🖨️ 
+      </span>
+    </h1>
     <GoogleMap 
       mapContainerStyle = {mapContainerStyle} 
       zoom = {8} 
       center = {center}
       options={options}
+      onClick={onMapClick}
+      onLoad={onMapLoad}
       >
+        {markers.map(marker => (
+          <Marker 
+          key={LoginForm.email} 
+          position = {{lat: marker.lat, lng: marker.lng}}
+          icon={{
+            url: '/printer.png',
+            scaledSize: new window.google.maps.Size(30, 30),
+            origin: new window.google.maps.Point(0,0),
+            anchor: new window.google.maps.Point(15, 15)
+          }}
+          onClick={() => {
+            setSelected(marker);
+          }}
+          />
+        ))}
 
+        {selected? (
+        <InfoWindow 
+          position={{lat: selected.lat, lng: selected.lng}} 
+          onCloseClick={() => {
+            setSelected(null) 
+          }}
+        >
+          <div>
+            <h2>Team Printer</h2>
+            <p>Team Info</p>
+            <p>Location</p>
+          </div>
+        </InfoWindow>) : null}
     </GoogleMap>
   </div>
   );
