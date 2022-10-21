5b780a81f2d2a79a3279554a02f734b16bb7cb32
maartoon <85377718+maartoon@users.noreply.github.com>
2022-10-21 01:26:55 -0700

    eh

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
+import { useRef } from 'react';
 import styles from '../Home/home.module.css'
 import Controls from '../../components/actions/Controls'
+import ScrollButton from 'react-scroll-button';
 
-export default class Home extends Component {
+export default function Home(){
+  const ref = useRef(null);
+  const handleScrollDown = () => {
+    ref.current?.scrollIntoView({behavior: 'smooth'});
+  }
 
-  render() {
   return(
       <body>
           <div className={styles.StartScreen}>
             <div className={styles.block}>
-              <h1 className={styles.title}>Zippyprints</h1>
-              <p className={styles.subtitle}>A fast, easy to use, and reliable way for printing custom designs through printers near you!</p>
-              <div className={styles.button}>
-              <div className={styles.buttonText}>Login</div>  
+              <h1 className={styles.title}>ZippyPrints</h1>
+              <p className={styles.subtitle}>A fast, easy to use, and reliable way for 3D printing custom designs through printers near you!</p>
+              <button
+              className={styles.scrollButton}
+              onClick={handleScrollDown}
+              >
+                <div className={styles.buttonText}>Learn More</div>
+              </button>      
+            </div>
+            <div ref={ref} className={styles.tutorialBackground}>
+            <div sx={{height: 1020}} className={styles.redVertical} />
+              <h1 className={styles.tutorialTitle}>How It Works</h1>
+              <p className={styles.tutorialSubtitle}>For Printers:</p>
+                <p className={styles.tutorialDescription}>- Register an account</p>
+                <p className={styles.tutorialDescription}>- Fill out the necessary information on your profile!</p>
+                <p className={styles.tutorialDescription}>- Make an estimation on how much you want to charge ($/cm<sup>3</sup>)!</p>
+                <p className={styles.tutorialDescription}>- When a team reuqests a print, you will recieve an email with their</p>
+                <p className={styles.tutorialDescription}>‏‏‎ ‎‏‏‎ ‎contact information and any additional information!</p>
+                <p className={styles.tutorialDescription}>- Now, simply wait for requests to flow in</p>
+            <div className={styles.divider}></div>   
+                <div sx={{height: 1020}} className={styles.teamVertical} />
+                  <p className={styles.teamTutorialSubtitle}>For Teams:</p>
+                  <p className={styles.teamTutorialDescription}>- Register an account</p>
+                  <p className={styles.teamTutorialDescription}>- Fill out the necessary information on your profile!</p>
+                  <p className={styles.teamTutorialDescription}>- Make an estimation on how much you want to charge ($/cm<sup>3</sup>)!</p>
+                  <p className={styles.teamTutorialDescription}>- When a team reuqests a print, you will recieve an email with their</p>
+                  <p className={styles.teamTutorialDescription}>‏‏‎ ‎‏‏‎ ‎contact information and any additional information!</p>
+                  <p className={styles.teamTutorialDescription}>- Now, simply wait for requests to flow in</p>
+
               </div>
-              </div>
+          
           </div>
+            
         </body>
 
       );
-}
+
 }
 
